14c7d018fb4d6866426ec67d3a12d5b59a60166f
mauriceoboya <mauriq97@gmail.com>
2023-09-11 02:08:31 +0300

    updated

--- sql_brushoff.sql
+++ sql_brushoff.sql
@@ -1,25 +1,12 @@
 show databases;
-CREATE  DATABASE IF NOT EXISTS sales;
-USE sales;
-CREATE TABLE IF NOT EXISTS customers(
-customer_id INT PRIMARY KEY NOT NULL,
-first_name VARCHAR(20),
-second_name VARCHAR(20),
-email VARCHAR(20)
-);
+create database IF NOT EXISTS Customers;
 
-CREATE TABLE IF NOT EXISTS product(
-product_id INT PRIMARY KEY,
-product VARCHAR(50),
-price INT
-);
-
-CREATE TABLE IF NOT EXISTS orders(
-order_id INT PRIMARY KEY,
-orders VARCHAR(200),
-product_id INT,
-customer_id INT,
-FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-FOREIGN KEY (product_id) REFERENCES product(product_id),
-order_date  DATETIME DEFAULT NOW() 
+CREATE  TABLE IF NOT EXISTS Sales(
+CustomerID INT,
+CustomerName Varchar(200),
+ContactName Varchar(200),
+Address Varchar(200),
+City Varchar(200),
+PostalCode varchar(100),
+Country varchar (100)
 );
