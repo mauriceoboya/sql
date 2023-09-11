502986be6755c6c3247c188c8813646156e63b47
mauriceoboya <mauriq97@gmail.com>
2023-09-11 03:18:18 +0300

    updated

--- sql_brushoff.sql
+++ sql_brushoff.sql
@@ -1,6 +1,6 @@
 show databases;
 create database IF NOT EXISTS Customers;
-
+Use  Customers;
 CREATE  TABLE IF NOT EXISTS Sales(
 CustomerID INT,
 CustomerName Varchar(200),
@@ -10,3 +10,14 @@ City Varchar(200),
 PostalCode varchar(100),
 Country varchar (100)
 );
+
+INSERT INTO  Sales VALUES(
+1,'Afreds Futterkiste','Maria Anders','Obere Str. 57','Berlin','12209','Germany'),
+(2,'Ana Trujillo Emparedados y Helados','Ana  Trujilo',' Ava de la 222','Mexico D.F','0032','Mexico')
+
+SELECT * FROM Sales;
+#The WHERE clause is used to filter records.
+SELECT * FROM Sales WHERE  Country ='Germany';
+#distinct statement is used to return only unique (different) values.
+INSERT INTO Sales Values()
+SELECT  DISTINCT  Address,City,PostalCode from Sales;
